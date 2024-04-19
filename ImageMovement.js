aae02112ec0ca73fcb1240e1352164efb8cca341
Víctor Romero <virn2005@gmail.com>
2024-04-19 17:02:12 -0600

    Some Images & Feel Upgrades

---
+++ ImageMovement.js
@@ -0,0 +1,32 @@
+const track = document.getElementById("img-track")
+
+window.onmousedown = e => {
+    TrackEvent.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate({
+        transform: `translate(${nextPercentage}, -50%)`
+    }, {duration: 1200, fill: "forwards"});
+}
+
+for (const image of track.getElementsByClassName("img-track")) {
+    image.animate({
+        objectPosition: `${100 + nextPercentage}, center`
+    }, {duration: 1200, fill: "forwards"});
+}
